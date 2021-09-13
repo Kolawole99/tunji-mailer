c60b40e31ca83c78eea4d29227033304cd16e7d2
Kolawole Ojo <philliphopeojo@gmail.com>
2021-09-13 13:02:14 +0100

    TEST: Query utility completed

--- test/utilities/query.js
+++ test/utilities/query.js
@@ -1,10 +1,8 @@
-/** */
-const { assert, expect } = require('chai');
-const util = require("../../src/utilities/query");
+const { expect } = require('chai');
+const util = require('../../src/utilities/query');
 
-describe("Tests Query utility", () => {
-
-    it("builds correct in data", () => {
+describe('Tests Query utility', () => {
+    it('builds correct in data', () => {
         const string = '14:15:89';
         const needle = string.split(':')[0];
         expect(util.buildInQuery(string))
@@ -13,7 +11,7 @@ describe("Tests Query utility", () => {
             .that.contains(needle);
     });
 
-    it("builds correct nor data", () => {
+    it('builds correct nor data', () => {
         const string = '!15!89';
         const needle = string.split('!')[1];
         expect(util.buildNorQuery(string))
@@ -22,7 +20,7 @@ describe("Tests Query utility", () => {
             .that.contains(needle);
     });
 
-    it("builds correct or data", () => {
+    it('builds correct or data', () => {
         const string = '14,15,89';
         const needle = string.split(',')[0];
         expect(util.buildOrQuery(string))
@@ -31,34 +29,34 @@ describe("Tests Query utility", () => {
             .that.contains(needle);
     });
 
-    it("builds correct data $gte", () => {
-        const string = "2~15";
-        const expected_value = Number(string.split('~')[0]);
+    it('builds correct data $gte', () => {
+        const string = '2~15';
+        const expectedValue = Number(string.split('~')[0]);
         expect(util.buildRangeQuery(string))
             .to.be.an('object')
             .to.have.property('$gte')
-            .to.be.equal(expected_value);
+            .to.be.equal(expectedValue);
     });
 
-    it("builds correct data $lte", () => {
-        const string = "2~15";
-        const expected_value = Number(string.split('~')[1]);
+    it('builds correct data $lte', () => {
+        const string = '2~15';
+        const expectedValue = Number(string.split('~')[1]);
         expect(util.buildRangeQuery(string))
             .to.be.an('object')
             .to.have.property('$lte')
-            .to.be.equal(expected_value);
+            .to.be.equal(expectedValue);
     });
 
-    it("builds return_fields query params", () => {
-        const string = "name, age";
+    it('builds return_fields query params', () => {
+        const string = 'name, age';
         expect(util.buildReturnFieldsString(string))
             .to.be.a('string')
             .to.be.equal(string.replace(/,/gi, ' '))
             .to.not.contain(',');
     });
 
-    it("builds sort_by query params", () => {
-        const string = "name, age";
+    it('builds sort_by query params', () => {
+        const string = 'name, age';
         expect(util.buildSortOrderString(string))
             .to.be.a('string')
             .to.be.equal(string.replace(/,/gi, ' '))
@@ -66,44 +64,47 @@ describe("Tests Query utility", () => {
             .and.not.contain(',');
     });
 
-    it("builds wildcard options:", () => {
-        const key_list = 'firstname,lastname';
+    it('builds wildcard options:', () => {
+        const keyList = 'firstName,lastName';
         const value = 'Nathan';
 
-        expect(util.buildWildcardOptions(key_list, value))
+        expect(util.buildWildcardOptions(keyList, value))
             .to.have.ownProperty('$or')
             .to.be.an('array')
-            .has.length(key_list.split(',').length);
+            .has.length(keyList.split(',').length);
     });
 
-    it("determines correct pagination", () => {
-        const page = 1, population = 50;
+    it('determines correct pagination', () => {
+        const page = 1;
+        const population = 50;
         const skip = page * population;
         expect(util.determinePagination(page, population))
-            .to.have.keys(["limit", "skip"])
-            .to.have.property("skip")
+            .to.have.keys(['limit', 'skip'])
+            .to.have.property('skip')
             .to.be.equal(skip);
     });
 
-    it("returns correct data", () => {
+    it('returns correct data', () => {
         const keys = [
-            'count', 'fields_to_return', 'limit',
-            'seek_conditions', 'skip', 'sort_condition'
+            'count',
+            'fieldsToReturn',
+            'limit',
+            'seekConditions',
+            'skip',
+            'sortCondition',
         ];
 
         const options = {
-            firstname: 'matt,nate',
-            lastname: '!David',
-            age: "13:15:67",
-            score: "50~100",
-            sort_by: '-firstname',
-            return_only: 'firstname,lastname',
+            firstName: 'matt,nate',
+            lastName: '!David',
+            age: '13:15:67',
+            score: '50~100',
+            sortBy: '-firstname',
+            returnOnly: 'firstname,lastname',
             page: 0,
             population: 100,
-        }
+        };
 
-
-        expect(util.buildQuery(options))
-            .to.have.keys(keys);
+        expect(util.buildQuery(options)).to.have.keys(keys);
     });
 });
